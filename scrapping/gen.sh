1611699cd1213c01a7559804ef67d9d50baf4b64
pazamelin <zamelinpavel@gmail.com>
2020-02-27 03:29:38 +0300

    del

--- scrapping/gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Usage: bash scripts/gen.sh
-
-sort -u 1k.txt -o 1k.txt
-cp 1k.txt out_alpha.txt
-grep -v '[[:out_alpha:]]*' out_alpha.txt
-python3 scripts/main.py 1k.txt > test_1k_out.xml
-rm *.zip
-find . -type f -name "test_1k_out*" -maxdepth 1 -execdir zip '{}.zip' '{}' \;
--- scrapping/main.py
+++
@@ -1,5 +0,0 @@
-import files
-from scrape import requestCD, requestUD, requestCD_subItem
-
-res = requestCD("burst")
-files.dumpWord("test.xml", res)
